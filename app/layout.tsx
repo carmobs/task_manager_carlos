4c7e1aaefb0960158f27c327e1fd8737a79e745f
CarlosGomez <carmobs>
2025-04-30 04:04:06 -0600

    Revert "feat: add dynamic rendering to Dashboard and improve layout structure"
    This reverts commit c4c11281b168a0072013f486b67d11bf36da0b0f.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // Importa los estilos globales
+import "./globals.css"; // Asegúrate de que este archivo esté correctamente importado
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
-      <body>{children}</body>
+    <html lang="en">
+      <head>
+        <style>{`html { font-family: ${inter.style.fontFamily}; }`}</style>
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
