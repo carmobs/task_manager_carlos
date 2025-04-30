08601c339c8d9dbc399292d8d579e9403c5ce55b
CarlosGomez <carmobs>
2025-04-29 23:15:26 -0600

    feat: update layout and globals for improved styling and add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Incluye todos los archivos en la carpeta app
+    "./components/**/*.{js,ts,jsx,tsx}", // Incluye todos los archivos en la carpeta components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
